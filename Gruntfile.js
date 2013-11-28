e3128fb767c7196f6f914d5acffb4d501765ff1f
Florent Viel <fviel@wanadev.fr>
2013-11-28 15:07:20 +0100

    Add: npm + bower + grunt

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist_js: {
+        src: [
+          'bower_components/jquery/jquery.js',
+          'bower_components/bootstrap/dist/js/bootstrap.js'
+        ],
+        dest: 'public/js/<%= pkg.name %>.js'
+      },
+      dist_css: {
+        src: [
+          'bower_components/bootstrap/dist/css/bootstrap.css',
+          'bower_components/font-awesome/css/font-awesome.css',
+          'assets/css/app.css'
+        ],
+        dest: 'public/css/<%= pkg.name %>.css'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist_js.dest %>',
+        dest: 'public/js/<%= pkg.name %>.min.js'
+      }
+    },
+    cssmin: {
+      options: {
+        keepSpecialComments: 0
+      },
+      dist: {
+        src: '<%= concat.dist_css.dest %>',
+        dest: 'public/css/<%= pkg.name %>.min.css'
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: 'bower_components/font-awesome/fonts/**',
+            dest: 'public/fonts/',
+            filter: 'isFile'
+          }
+        ]
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-css');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task.
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'copy']);
+
+};
