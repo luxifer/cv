eb1f552fa3b073c1f17f62bd7e68ee6534730571
Florent Viel <fviel@wanadev.fr>
2013-12-06 09:03:33 +0100

    Mod: finalize server.go

--- server.go
+++ server.go
@@ -1,78 +1,80 @@
 package main
 
 import (
-    "bytes"
-    "github.com/codegangsta/martini"
-    "github.com/codegangsta/martini-contrib/form"
-    "github.com/codegangsta/martini-contrib/render"
-    "github.com/russross/blackfriday"
-    "github.com/sendgrid/sendgrid-go"
-    "html/template"
-    "io/ioutil"
-    "os"
-    "path/filepath"
+	"bytes"
+	"fmt"
+	"github.com/codegangsta/martini"
+	"github.com/codegangsta/martini-contrib/binding"
+	"github.com/codegangsta/martini-contrib/render"
+	"github.com/russross/blackfriday"
+	"github.com/sendgrid/sendgrid-go"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-    m := martini.Classic()
-    m.Use(render.Renderer("templates"))
-    if martini.Env == martini.Dev {
-        m.Use(martini.Static("public"))
-    }
+	m := martini.Classic()
+	m.Use(render.Renderer("templates"))
+	if martini.Env == martini.Dev {
+		m.Use(martini.Static("public"))
+	}
 
-    content := parseMarkdown("data")
+	content := parseMarkdown("data")
 
-    m.Get("/", func(r render.Render) {
-        r.HTML(200, "index", template.HTML(content))
-    })
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "index", template.HTML(content))
+	})
 
-    m.Post("/contact", form.Form(&Contact{}), func(contact *Contact) (int, string) {
-        sg := sendgrid.NewSendGridClient(os.Getenv("SENDGRID_USER"), os.Getenv("SENDGRID_KEY"))
-        message := sendgrid.NewMail()
-        message.AddTo(os.Getenv("SENDGRID_EMAIL"))
-        message.AddSubject(contact.Object)
-        message.AddText(contact.Content)
-        message.AddFrom(contact.From)
+	m.Post("/contact", binding.Bind(Contact{}), func(contact Contact) (int, string) {
+		body := fmt.Sprintf("%s\n\nEnvoyé depuis http://florentviel.com", contact.Content)
+		sg := sendgrid.NewSendGridClient(os.Getenv("SENDGRID_USER"), os.Getenv("SENDGRID_KEY"))
+		message := sendgrid.NewMail()
+		message.AddTo(os.Getenv("SENDGRID_EMAIL"))
+		message.AddSubject(contact.Object)
+		message.AddText(body)
+		message.AddFrom(contact.From)
 
-        r := sg.Send(message)
+		r := sg.Send(message)
 
-        if r == nil {
-            return 200, "ok"
-        }
+		if r == nil {
+			return 200, "ok"
+		}
 
-        return 500, "ko"
-    })
+		return 500, "ko"
+	})
 
-    m.Run()
+	m.Run()
 }
 
 type Contact struct {
-    From    string `form:"from,required"`
-    Object  string `form:"object,required"`
-    Content string `form:"content,required"`
+	From    string `form:"from" required`
+	Object  string `form:"object" required`
+	Content string `form:"content" required`
 }
 
 func parseMarkdown(dir string) string {
-    var content bytes.Buffer
+	var content bytes.Buffer
 
-    filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-        if _, err := filepath.Rel(dir, path); err != nil {
-            return err
-        }
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if _, err := filepath.Rel(dir, path); err != nil {
+			return err
+		}
 
-        if !info.IsDir() {
-            buf, err := ioutil.ReadFile(path)
-            if err != nil {
-                panic(err)
-            }
+		if !info.IsDir() {
+			buf, err := ioutil.ReadFile(path)
+			if err != nil {
+				panic(err)
+			}
 
-            content.WriteString(string(buf))
-        }
+			content.WriteString(string(buf))
+		}
 
-        return nil
-    })
+		return nil
+	})
 
-    markdown := blackfriday.MarkdownBasic(content.Bytes())
+	markdown := blackfriday.MarkdownBasic(content.Bytes())
 
-    return string(markdown)
+	return string(markdown)
 }
