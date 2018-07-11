4736f0dde423cb0209015185f49a9ac8af61ba25
Florent Viel <luxifer666@gmail.com>
2018-07-11 09:16:53 +0200

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,7 @@
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
 var cssmin = require('gulp-cssmin');
-
-gulp.task('js', function() {
-    gulp.src([
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/bootstrap/dist/js/bootstrap.js',
-            'assets/js/app.js'
-        ])
-        .pipe(concat('cv.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('public/js'));
-});
 
 gulp.task('less', function() {
     return gulp.src('assets/less/app.less')
@@ -23,8 +11,6 @@ gulp.task('less', function() {
 
 gulp.task('css', ['less'], function() {
     return gulp.src([
-            'bower_components/bootstrap/dist/css/bootstrap.css',
-            'bower_components/font-awesome/css/font-awesome.css',
             'assets/css/app.css'
         ])
         .pipe(concat('cv.min.css'))
@@ -32,11 +18,6 @@ gulp.task('css', ['less'], function() {
         .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('data', function() {
-    gulp.src('bower_components/font-awesome/fonts/*')
-        .pipe(gulp.dest('public/fonts'));
-});
-
-gulp.task('default', ['js', 'less', 'css', 'data'], function() {
+gulp.task('default', ['less', 'css'], function() {
     // place code for your default task here
 });
